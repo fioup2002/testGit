36a51fcaef77d124afeb44d8a098dbe7fcc49c2a
Ice.Liu <ice.liu@ihwic.com>
2021-03-31 10:43:10 +0800

    測試

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <nav>
+        <router-link to='/'>Home</router-link>
+        <router-link to='/register'>Register</router-link>
+        <router-link to='/login'>Login</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+
+<style lang="scss" src="./assets/css/test.scss"></style>
+
+<script>
+
+export default {
+}
+</script>
